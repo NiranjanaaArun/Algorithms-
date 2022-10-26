4108bf75f6f4530abac521f3237beaa908f0075f
niranjanaaarun <niranjanaa.arun@gmail.com>
2022-10-26 12:08:04 -0500

    added1

--- Algorithm_Hw1/odd_even_digits_within_number.py
+++ Algorithm_Hw1/odd_even_digits_within_number.py
@@ -1,18 +1,19 @@
-def odd_even_digit(a):
-    c=a
-    b=0
-    while b == a%10:
-        if b % 2 == 0:
-            print (b, "is a even number")
-        elif b % 2 != 0:
-            print(b, "is an odd number")
+def sum_of_digits(n):
+    odds = 0
+    even = 0
+    while n > 0:
+        current_digit = n % 10
+        if current_digit % 2:
+            odds = odds + 1
         else:
-            break
-    c= c//10
+            even = even + 1
+        n = n//10
+    return [odds, even]
 
-number = 514235713
-odd_even_digit(number)
-print (c)
+test_n = 13454698
+result = sum_of_digits(test_n)
+print(result)
+
 
 
 
--- Algorithm_Hw1/sum_of_digits.py
+++ Algorithm_Hw1/sum_of_digits.py
@@ -1,16 +1,13 @@
-def sum_of_digits(n):
-    odds = 0
-    even = 0
-    while n > 0:
-        current_digit = n % 10
-        if current_digit % 2:
-            odds = odds + 1
-        else:
-            even = even + 1
-        n = n//10
-    return [odds, even]
+def sum_digits(num):
+    a = 0
+    sum = 0
+    while( a < num+1):
+        sum+=a
+        a+=1
+    return (sum)
 
-test_n = 13454698
-result = sum_of_digits(test_n)
-print(result)
+total_of = 8
+final_result = sum_digits(total_of)
+print(final_result)
+
 
