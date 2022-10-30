4298cea85959abed1ff7aa292922937a0987e108
niranjanaaarun <niranjanaa.arun@gmail.com>
2022-10-30 09:04:38 -0500

    added new algorithm

---
+++ Algorithm_Hw2/first_unique_character_string.py
@@ -0,0 +1,36 @@
+# return the first unique, if there are no unique characters return "xoxo"
+
+#def first_unique_character(string: str):
+#    string=string.lower()
+#    for l in string:
+#        if string.count(l) == 1:
+#            return l
+
+#print(first_unique_character(""))
+
+
+def unique(string1: str):
+    string1=string1.lower()
+    d = {}
+    e = "xoxo"
+    for letter in string1: #google
+        if letter not in d:
+            d[letter] = 1
+        else:
+            d[letter] += 1
+    print (d)
+
+    for k, v in d.items(): #when we go through a dictionary from above, and try to get values from that, then
+        if v==1 :
+            return k
+
+    return ""
+print(unique("xoxo"))
+
+
+
+
+
+
+
+
